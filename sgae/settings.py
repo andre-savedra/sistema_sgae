a76457e698ca0fa371b2b90de21d0a209cd69c5e
andre-savedra <andresavedracruz@gmail.com>
2022-03-09 02:03:45 -0300

    fixed_security

--- sgae/settings.py
+++ sgae/settings.py
@@ -98,8 +98,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sgae.mange@gmail.com'
-EMAIL_HOST_PASSWORD = '@senaimange501'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Database
