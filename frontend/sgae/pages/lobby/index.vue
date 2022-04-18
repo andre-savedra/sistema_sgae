0b7cae83f6d7be3ecabb10aea8953493516712d5
andre-savedra <andresavedracruz@gmail.com>
2022-04-18 13:16:52 -0300

    fixing_lobby_problem_4

--- frontend/sgae/pages/lobby/index.vue
+++ frontend/sgae/pages/lobby/index.vue
@@ -5,7 +5,10 @@
         <img v-if="flip" class="back" src="@/assets/media/sgae.png" />
         <img v-else class="front" src="@/assets/media/sgae.png" />
       </div>
-      <h1 v-if="loggedUser.nome">&#10024; {{ loggedUser.nome.split(' ')[0] }}, preparando seu ambiente! &#10024;</h1>
+      <h1 v-if="loggedUser.nome">
+        &#10024; {{ loggedUser.nome.split(" ")[0] }}, preparando seu ambiente!
+        &#10024;
+      </h1>
       <h1 v-else>&#10024; Preparando seu ambiente! &#10024;</h1>
     </section>
     <footer class="p-d-flex p-flex-column p-jc-center p-ai-end">
@@ -27,45 +30,72 @@ export default {
     return {
       flip: false,
       storaged: false,
-      counter: 0
+      counter: 0,
     };
   },
   methods: {
     flipper: function () {
-      if(this.loggedUser !== null && 
-         this.loggedUser !== undefined) {
-          this.counter++;
+      if (this.loggedUser !== null && this.loggedUser !== undefined) {
+        this.counter++;
       }
 
       // console.log("flipper");
 
-        const flipperTimer = setTimeout(() => {
-          this.flip = !this.flip;
-          this.flipper();
-          // console.log("this.$store.state.actualUser");
-          // console.log(this.$store.state.actualUser);
-        }, 1000);
-        if(this.counter > 4){
-          clearTimeout(flipperTimer);
-          // console.log("redirecionando....");
-          // this.$router.push("/initial");
-          this.$router.push("/tarefas");
-        }
-      
+      const flipperTimer = setTimeout(() => {
+        this.flip = !this.flip;
+        this.flipper();
+        // console.log("this.$store.state.actualUser");
+        // console.log(this.$store.state.actualUser);
+      }, 1000);
+      if (this.counter > 4) {
+        clearTimeout(flipperTimer);
+        // console.log("redirecionando....");
+        // this.$router.push("/initial");
+        this.$router.push("/tarefas");
+      }
     },
-   
   },
   mounted() {
     // console.log("lobby page...");
     this.flipper();
   },
+  async asyncData({ $axios, $auth }) {
+    //cheack if there is value storaged
+    const valueStoraged = $auth.$storage.getUniversal("actualUserStoraged");
+    if (valueStoraged) {
+      console.log("Storaged already defined");
+      console.log(valueStoraged);
+    }
 
- 
+    const server = process.env.BASE_URL + "/";
+    const users = await $axios.get(server + "api/v1/users/me/");
+    // console.log("users");
+    // console.log(users);
+
+    let loggedUser = null;
+
+    if (users) {
+      const usuarios = await $axios.get(server + "usuarios/" + users.data.id);
+      // console.log("usuarios");
+      // console.log(usuarios);
+      loggedUser = usuarios.data.data;
+    }
+
+    // console.log("user async");
+    // console.log(loggedUser);
+
+    if (loggedUser) {
+      await $auth.$storage.setUniversal("actualUserStoraged", loggedUser);
+      return {
+        loggedUser,
+      };
+    }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
- .lobby {
+.lobby {
   width: 100vw;
   height: 100vh;
 
@@ -137,5 +167,5 @@ export default {
   p {
     font-size: 11px;
   }
-} 
+}
 </style>
