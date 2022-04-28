7cba19f024502ff7b4dc2d076e083cd4a188b1a4
andre-savedra <andresavedracruz@gmail.com>
2022-04-28 16:13:51 -0300

    ios_problem_1

--- frontend/sgae/pages/lobby/index.vue
+++ frontend/sgae/pages/lobby/index.vue
@@ -64,7 +64,8 @@ export default {
         // .get("https://backend.sgae501.com.br/usuarios/2")
         fetch("https://backend.sgae501.com.br/usuarios/2", {
           headers: {
-            Authorization: this.$auth.$storage.getUniversal("_token.local"),
+            // Authorization: this.$auth.$storage.getUniversal("_token.local"),
+            Authorization: 'Token 6386db97cafdf17280cd5c3e12d71f98560efe66',
           },
         })
         .then(async (usuarios) => {
